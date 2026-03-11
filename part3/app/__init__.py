fbfa49daffb209191a1220dabeb2a695f02ea020
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-11 15:33:02 +0100

    Add - jwt authentication with login endpoint and protected route

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     api = Api(
         app,
@@ -18,16 +21,18 @@ def create_app(config_class="config.DevelopmentConfig"):
         doc='/api/v1/'
     )
 
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.bookings import api as bookings_ns
+    from app.api.v2.users import api as users_ns
+    from app.api.v2.amenities import api as amenities_ns
+    from app.api.v2.places import api as places_ns
+    from app.api.v2.reviews import api as reviews_ns
+    from app.api.v2.bookings import api as bookings_ns
+    from app.api.v2.auth import api as auth_ns
 
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(bookings_ns, path='/api/v1/bookings')
+    api.add_namespace(users_ns, path='/api/v2/users')
+    api.add_namespace(amenities_ns, path='/api/v2/amenities')
+    api.add_namespace(places_ns, path='/api/v2/places')
+    api.add_namespace(reviews_ns, path='/api/v2/reviews')
+    api.add_namespace(bookings_ns, path='/api/v2/bookings')
+    api.add_namespace(auth_ns, path='/api/v2/auth')
 
     return app
