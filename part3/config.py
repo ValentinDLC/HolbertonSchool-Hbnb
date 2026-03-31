50bc0c5c020289fb1ff6902eb8129f179a5bb79a
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-31 12:03:39 +0200

    Fix - config - remove Bearer prefix requirement for JWT in Swagger

--- part3/config.py
+++ part3/config.py
@@ -1,5 +1,6 @@
-import os
 from datetime import timedelta
+import os
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
@@ -9,9 +10,8 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/hbnb_prod'
+    JWT_HEADER_TYPE = ''
+    JWT_HEADER_NAME = 'Authorization'
 
 config = {
     'development': DevelopmentConfig,
