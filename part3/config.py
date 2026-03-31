3182822763e321c73d8e8b2c7afe1f0ae9c60bf4
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-31 10:36:45 +0200

    Feat - config  - extend JWT token expiry to 24h for development

--- part3/config.py
+++ part3/config.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import timedelta
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
@@ -8,6 +8,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/hbnb_prod'
