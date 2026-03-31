9e9fe1eebefb95b9c1db17b2451ceabd668fa4e1
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-31 11:57:25 +0200

    Fix - swagger - remove Bearer prefix requirement in Authorize field

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -8,6 +8,7 @@ bcrypt = Bcrypt()
 jwt = JWTManager()
 db = SQLAlchemy()
 
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -21,7 +22,6 @@ def create_app(config_class="config.DevelopmentConfig"):
             'type': 'apiKey',
             'in': 'header',
             'name': 'Authorization',
-            'description': "Entrez: 'Bearer <votre_token>'"
         }
     }
 
@@ -35,18 +35,18 @@ def create_app(config_class="config.DevelopmentConfig"):
         security='Bearer Auth'
     )
 
-    from app.api.v2.users import api as users_ns
+    from app.api.v2.users     import api as users_ns
     from app.api.v2.amenities import api as amenities_ns
-    from app.api.v2.places import api as places_ns
-    from app.api.v2.reviews import api as reviews_ns
-    from app.api.v2.bookings import api as bookings_ns
-    from app.api.v2.auth import api as auth_ns
+    from app.api.v2.places    import api as places_ns
+    from app.api.v2.reviews   import api as reviews_ns
+    from app.api.v2.bookings  import api as bookings_ns
+    from app.api.v2.auth      import api as auth_ns
 
-    api.add_namespace(users_ns, path='/api/v2/users')
+    api.add_namespace(users_ns,     path='/api/v2/users')
     api.add_namespace(amenities_ns, path='/api/v2/amenities')
-    api.add_namespace(places_ns, path='/api/v2/places')
-    api.add_namespace(reviews_ns, path='/api/v2/reviews')
-    api.add_namespace(bookings_ns, path='/api/v2/bookings')
-    api.add_namespace(auth_ns, path='/api/v2/auth')
+    api.add_namespace(places_ns,    path='/api/v2/places')
+    api.add_namespace(reviews_ns,   path='/api/v2/reviews')
+    api.add_namespace(bookings_ns,  path='/api/v2/bookings')
+    api.add_namespace(auth_ns,      path='/api/v2/auth')
 
     return app
